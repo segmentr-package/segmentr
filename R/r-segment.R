8494ba6a0069f7ee71714fa1ec4d4ff396854f0e
Thales Mello <thalesmello@gmail.com>
2018-04-10 14:21:25 -0300

    Reindent functions

--- R/r-segment.R
+++ R/r-segment.R
@@ -4,12 +4,12 @@
 #' @example
 r_multivariate <- function(X)
 {
-	ip <- table( apply(as.matrix(X),1,paste0,collapse="") )
-	n <- nrow(X)
-	pip <- ip/sum(ip)
-	loglik <-  sum( ip*log(pip) )
-	df <- length(pip)
-	loglik
+  ip <- table( apply(as.matrix(X),1,paste0,collapse="") )
+  n <- nrow(X)
+  pip <- ip/sum(ip)
+  loglik <-  sum( ip*log(pip) )
+  df <- length(pip)
+  loglik
 }
 
 
@@ -18,34 +18,34 @@ r_multivariate <- function(X)
 #' @export
 segment <- function(x,segmax=ncol(x),loglikfun=multivariate,c1=1)
 {
-	m <- ncol(x)
-	n <- nrow(x)
-#	alf <- length(levels(factor(x)))
-	z <- matrix(ncol=m,nrow=segmax)
-	r <- matrix(ncol=m,nrow=segmax-1)
-	for(i in 1:m){
-		w <- loglikfun( as.matrix(x[,1:i]) )
-		z[1,i] <- w[1] + c1*sqrt(n)# - c2*(w[2]-1)
-	}
-	for(k in 2:segmax){
-		for(i in k:m){
-			q <- c()
-			for(j in (k-1):(i-1)){
-				w <- loglikfun( as.matrix(x[,(j+1):i]) )
-				q <- append(q, z[k-1,j] + w[1] + c1*sqrt(n))# - c2*(w[2]-1) )
-			}
-			z[k,i] <- max(q)
-			r[k-1,i] <- which.max(q) + k - 2
-		}
-	}
-#	print(z[,m])
-	segshat <- which.max(z[,m])
-	if (segshat > 1){
-		segs <- r[segshat-1,m]
-		if( segshat > 2 ) for(k in (segshat-1):2) segs <- c(r[k-1,segs[1]],segs)
-	}
-	else segs <- c()
-	segs
+  m <- ncol(x)
+  n <- nrow(x)
+  #	alf <- length(levels(factor(x)))
+  z <- matrix(ncol=m,nrow=segmax)
+  r <- matrix(ncol=m,nrow=segmax-1)
+  for(i in 1:m){
+    w <- loglikfun( as.matrix(x[,1:i]) )
+    z[1,i] <- w[1] + c1*sqrt(n)# - c2*(w[2]-1)
+  }
+  for(k in 2:segmax){
+    for(i in k:m){
+      q <- c()
+      for(j in (k-1):(i-1)){
+	w <- loglikfun( as.matrix(x[,(j+1):i]) )
+	q <- append(q, z[k-1,j] + w[1] + c1*sqrt(n))# - c2*(w[2]-1) )
+      }
+      z[k,i] <- max(q)
+      r[k-1,i] <- which.max(q) + k - 2
+    }
+  }
+  #	print(z[,m])
+  segshat <- which.max(z[,m])
+  if (segshat > 1){
+    segs <- r[segshat-1,m]
+    if( segshat > 2 ) for(k in (segshat-1):2) segs <- c(r[k-1,segs[1]],segs)
+  }
+  else segs <- c()
+  segs
 }
 
 #' @export
