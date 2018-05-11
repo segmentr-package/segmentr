1dac53b5270068b120ec33df6e74b5e4e1f476d5
Thales Mello <thalesmello@gmail.com>
2018-05-11 01:57:22 -0300

    Refactor segment.cpp
    For this refactor, some parts of the scripts were extracted to separate
    inline functions, with the intent of documenting that portion of the
    code and also making the main `multivariate` function body be easier to
    read.

--- src/segment.cpp
+++ src/segment.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 using namespace Rcpp;
 
-struct compareByRows {
+struct compare_by_rows {
   bool operator()(const NumericMatrix::Row& a, const NumericMatrix::Row& b) const {
     if (a.size() != b.size()) {
       return a.size() < b.size();
@@ -19,24 +19,34 @@ struct compareByRows {
   }
 };
 
-typedef std::map<NumericMatrix::Row, int, compareByRows> rowComparisonMap;
+typedef std::map<NumericMatrix::Row, int, compare_by_rows> row_comparison_map;
 
-// [[Rcpp::export]]
-double cpp_multivariate(NumericMatrix x) {
-  rowComparisonMap myMap;
+inline row_comparison_map row_frequency_table(NumericMatrix x) {
+  row_comparison_map my_map;
 
   for (int i = 0; i < x.nrow(); i++) {
     NumericMatrix::Row row = x(i, _);
-    myMap[row]++;
+    my_map[row]++;
   }
 
-  // Extract values from the frequencies map
-  std::list<double> valueList;
-  for (rowComparisonMap::const_iterator it=myMap.begin(); it!=myMap.end(); ++it) {
-    valueList.push_back( it->second );
+  return my_map;
+}
+
+inline NumericVector extract_map_values(row_comparison_map& my_map) {
+  std::vector<int> values;
+
+  for (row_comparison_map::const_iterator it=my_map.begin(); it!=my_map.end(); ++it) {
+    values.push_back( it->second );
   }
 
-  NumericVector values(valueList.begin(), valueList.end());
+  NumericVector valuesVector(values.begin(), values.end());
+  return valuesVector;
+}
+
+// [[Rcpp::export]]
+double cpp_multivariate(NumericMatrix x) {
+  row_comparison_map my_map = row_frequency_table(x);
+  NumericVector values = extract_map_values(my_map);
   NumericVector partial_probs = values / sum(values);
   double loglik = sum(values * log(partial_probs));
   int df = values.size();
