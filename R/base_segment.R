f088163c0fb074458cc0c4446f9a3609e290b69a
Thales Mello <thalesmello@gmail.com>
2018-11-30 14:25:27 -0200

    Update base_segment parameter descriptions

--- R/base_segment.R
+++ R/base_segment.R
@@ -2,20 +2,28 @@
 #'
 #' Describe base arguments for segment function
 #'
-#' @param data matrix for which we wish to estimate the independent segments of.
-#' @param log_likelihood log likelihood estimation funciton, which will be
-#'   applied to all possible combinations of segments. Because it's executed
-#'   many times, it's likely to be the slow part of the function execution, so
+#' @param data matrix for which to find the changepooints
+#' @param log_likelihood a function receives the the segment matrix as argument
+#'   and returns a likelihood estimation. This function is used to calculate the
+#'   changepoints that maximize the total likelihood. Depending on the algorithm
+#'   being used, this function is likely to be executed many times, in which
+#'   case it's also likely to be the bottleneck of the function execution, so
 #'   it's advised that this function should have a performant, native
 #'   implementation. Defaults to a performant `multivariate` estimation.
-#' @param penalty a function that determines the penalty for the segment. It's
-#'   called with the segment being analysed as it's only parameter.
-#' @param initial_position a initial position for the recursive algorithm.
+#' @param penalty a function that receives the segment as parameter and returns
+#'   the penalty to the segment, directly subtracted from the likelihood
+#'   estimated by the other function. The idea if for this to be used as a way
+#'   of avoiding undesirable results, e.g. to avoid large segments by provind a
+#'   penalty function that penalizes big segments, of the other way arround, by
+#'   providing a penalty function that penalizes small segments.
+#' @param max_segments an integer that defines the maximum amount of segments to
+#'   split the data into.
 #' @param allow_parallel allows parallel execution to take place using the
-#'   registered cluster. Defaults to TRUE.
+#'   registered cluster. Assumes a cluster is registered with the `foreach`
+#'   package. Defaults to TRUE.
 base_segment <- function(
                          data,
                          log_likelihood,
                          penalty,
-                         initial_position,
+                         max_segments,
                          allow_parallel) NULL
--- R/segment.R
+++ R/segment.R
@@ -12,14 +12,14 @@
 #' @param algorithm can be of type `exact`, `hierarchical` or `hybrid`, Default: `exact`
 #' @return returns an object of type `segmentr`
 #' @examples
-#' 
+#'
 #' make_segment <- function(n, p) matrix(rbinom(100 * n, 1, p), nrow = 100)
 #' data <- cbind(make_segment(5, 0.1), make_segment(10, 0.9), make_segment(2, 0.1))
 #' mean_lik <- function(X) abs(mean(X) - 0.5) * ncol(X)^2
 #' segment(data, log_likelihood = mean_lik, algorithm = "hieralg")$changepoints
 #' @seealso [exactalg()] for the exact algorithm, [hieralg()] for the
-#'   hierarchical algorithm implementation, [hybridalg()] for the hybrid algorithm
-#'   implementation.
+#'   hierarchical algorithm implementation, [hybridalg()] for the hybrid
+#'   algorithm implementation.
 #'
 #' @export
 segment <- function(
