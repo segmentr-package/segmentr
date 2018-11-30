35f24227cf008d9b1870c3f7d665dd61dcd3ec0e
Thales Mello <thalesmello@gmail.com>
2018-11-30 14:07:07 -0200

    Include documentation for calculate_likelihood

--- R/calculate_likelihood.R
+++ R/calculate_likelihood.R
@@ -1,3 +1,17 @@
+#' Calculate a dataset's likelihood using changepoints of `segmentr` object
+#'
+#' Given the changepoints in a segmentr object, this function splits a
+#' new dataset into segments and then calculates the total likelihood
+#' using the log_likelihood function of the `segmentr` object.
+#'
+#' This function splits a `newdata` dataset into segments according to the
+#' changepoints in the `results` segmentr object. It then uses the `log_likelihood`
+#' function of the segmentr object to calculate the total likelihood of the new object.
+#'
+#'
+#' @param results a segmentr object, which contains the definition of the changepoints to be applied
+#' @param newdata a dataset for which we wish to calculate the total likelihood
+#'
 #' @export
 calculate_likelihood <- function(results, newdata) {
   likelihoods <- calculate_segment_likelihoods(results, newdata)
