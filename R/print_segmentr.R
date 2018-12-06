ee047b2cfc073f418aa3a72a2ecb9f5909a770d3
Thales Mello <thalesmello@gmail.com>
2018-12-06 19:05:45 -0200

    Fix print.segmentr documentation consistency

--- R/print_segmentr.R
+++ R/print_segmentr.R
@@ -5,23 +5,26 @@
 #' A short representation of the segments is printed on the screen, using the
 #' `start:end` range notation.
 #'
-#' @example
+#' @param x an object of type segmentr, containing changepoint information
+#' @param ... further arguments to be passed down to other methods
+#'
+#' @examples
 #' make_segment <- function(n, p) matrix(rbinom(100 * n, 1, p), nrow = 100)
 #' data <- cbind(make_segment(5, 0.1), make_segment(10, 0.9), make_segment(2, 0.1))
 #' mean_lik <- function(X) abs(mean(X) - 0.5) * ncol(X)^2
-#' results <- segment(data, log_likelihood = mean_lik, algorithm = "hieralg")
-#' print(results)
+#' x <- segment(data, log_likelihood = mean_lik, algorithm = "hieralg")
+#' print(x)
 #' @export
-print.segmentr <- function(results) {
-  starts <- sapply(results$segments, head, 1)
-  ends <- sapply(results$segments, tail, 1)
-  segment_strings <- foreach(start = starts, end = ends, .combine = c) %do% glue("{start}:{end}")
-  segment_strings <- glue_collapse(segment_strings, sep = "\n")
-  output <- glue("
-  Segments (total of {length(results$segments)}):
+print.segmentr <- function(x, ...) {
+  starts <- sapply(x$segments, head, 1)
+  ends <- sapply(x$segments, tail, 1)
+  segment_strings <- foreach(start = starts, end = ends, .combine = c) %do% glue::glue("{start}:{end}")
+  segment_strings <- glue::glue_collapse(segment_strings, sep = "\n")
+  output <- glue::glue("
+  Segments (total of {length(x$segments)}):
 
   {segment_strings}
   ")
 
-  print(output)
+  print(output, ...)
 }
