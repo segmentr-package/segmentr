a1089ac96595ea2e34551c8ecb7e603876b59294
Thales Mello <thalesmello@gmail.com>
2018-12-18 16:30:00 -0200

    Include missing calculate_likelihood parameter documentation

--- R/calculate_likelihood.R
+++ R/calculate_likelihood.R
@@ -11,6 +11,7 @@
 #'
 #' @param results a segmentr object, which contains the definition of the changepoints to be applied
 #' @param newdata a dataset for which we wish to calculate the total likelihood
+#' @param likelihood a likelihood function to be used to calculate the likelihood of each segment
 #'
 #' @export
 calculate_likelihood <- function(results, newdata, likelihood) {
