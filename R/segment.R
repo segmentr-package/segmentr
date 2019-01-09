6b4edd06edff6e4094bd72b17620c59684f9b3f0
Thales Mello <thalesmello@gmail.com>
2019-01-09 06:00:34 -0200

    Update example in segments documentation

--- R/segment.R
+++ R/segment.R
@@ -11,11 +11,11 @@
 #' @param ... other params to be passed to the underlying function
 #' @param algorithm can be of type `exact`, `hierarchical` or `hybrid`, Default: `exact`
 #' @examples
-#' 
+#'
 #' make_segment <- function(n, p) matrix(rbinom(100 * n, 1, p), nrow = 100)
 #' data <- cbind(make_segment(5, 0.1), make_segment(10, 0.9), make_segment(2, 0.1))
 #' mean_lik <- function(X) abs(mean(X) - 0.5) * ncol(X)^2
-#' segment(data, likelihood = mean_lik, algorithm = "hieralg")$changepoints
+#' segment(data, likelihood = mean_lik, algorithm = "hieralg")
 #' @seealso [exactalg()] for the exact algorithm, [hieralg()] for the
 #'   hierarchical algorithm implementation, [hybridalg()] for the hybrid
 #'   algorithm implementation.
